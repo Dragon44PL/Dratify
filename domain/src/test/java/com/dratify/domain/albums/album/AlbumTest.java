07aa1e2d15cfd5c54c6ad8c5793f4f26d6d7d610
Dragon44PL <krystian.pieniak44@gmail.com>
2021-05-24 22:08:27 +0200

    Renamed methods, changed test constants types

--- domain/src/test/java/com/dratify/domain/albums/album/AlbumTest.java
+++ domain/src/test/java/com/dratify/domain/albums/album/AlbumTest.java
@@ -14,12 +14,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class AlbumTest {
 
     private final UUID ALBUM_ID = UUID.randomUUID();
-    private final String ALBUM_NAME = "Test";
-    private final UUID ARTIST_ID = UUID.randomUUID();
-    private final UUID TRACK_ID = UUID.randomUUID();
+    private final String DEFAULT_ALBUM_NAME = "Test";
+    private final ArtistId DEFAULT_ARTIST = new ArtistId(UUID.randomUUID());
+    private final TrackId DEFAULT_TRACK = new TrackId(UUID.randomUUID());
+    private final TrackId ANOTHER_TRACK = new TrackId(UUID.randomUUID());
 
     private final String ANOTHER_ALBUM_NAME = "Another";
-    private final UUID ANOTHER_TRACK_ID = UUID.randomUUID();
 
     /**
      *  Album Events
@@ -33,26 +33,33 @@ class AlbumTest {
     @Test
     @DisplayName("Album Should Create Properly And Generate AlbumCreatedEvent")
     void albumShouldCreateProperlyAndGenerateEvent() {
-        final Album album = Album.create(ALBUM_ID, ALBUM_NAME, new ArtistId(ARTIST_ID), Set.of(new TrackId(TRACK_ID)));
-        Optional<AlbumEvent> albumEvent = album.findLatestEvent();
+        final Set<TrackId> TRACKS = Set.of(DEFAULT_TRACK);
+        final Album album = Album.create(ALBUM_ID, DEFAULT_ALBUM_NAME, DEFAULT_ARTIST, TRACKS);
 
+        Optional<AlbumEvent> albumEvent = album.findLatestEvent();
         assertTrue(albumEvent.isPresent());
         assertEquals(ALBUM_CREATED_EVENT, albumEvent.get().getClass());
+
+        final AlbumCreatedEvent albumCreatedEvent = (AlbumCreatedEvent) albumEvent.get();
+        assertEquals(ALBUM_ID, albumCreatedEvent.aggregateId());
+        assertEquals(DEFAULT_ALBUM_NAME, albumCreatedEvent.name());
+        assertEquals(DEFAULT_ARTIST, albumCreatedEvent.artist());
+        assertEquals(TRACKS, albumCreatedEvent.tracks());
     }
 
     @Test
     @DisplayName("Album Should Restore Properly And Not Generate Event")
     void albumShouldRestoreProperlyAndNotGenerateEvent() {
-        final Album album = Album.restore(ALBUM_ID, ALBUM_NAME, new ArtistId(ARTIST_ID), Set.of(new TrackId(TRACK_ID)));
-        Optional<AlbumEvent> albumEvent = album.findLatestEvent();
+        final Album album = Album.restore(ALBUM_ID, DEFAULT_ALBUM_NAME, DEFAULT_ARTIST, Set.of(DEFAULT_TRACK));
 
+        Optional<AlbumEvent> albumEvent = album.findLatestEvent();
         assertFalse(albumEvent.isPresent());
     }
 
     @Test
     @DisplayName("Album Name Should Be Changed When Different")
     void albumNameShouldChangeAndGenerateEvent() {
-        final Album album = Album.restore(ALBUM_ID, ALBUM_NAME, new ArtistId(ARTIST_ID), Set.of(new TrackId(TRACK_ID)));
+        final Album album = Album.restore(ALBUM_ID, DEFAULT_ALBUM_NAME, DEFAULT_ARTIST, Set.of(DEFAULT_TRACK));
         album.changeAlbumName(ANOTHER_ALBUM_NAME);
         Optional<AlbumEvent> albumEvent = album.findLatestEvent();
 
@@ -67,70 +74,67 @@ class AlbumTest {
     @Test
     @DisplayName("Album Name Should Not Be Changed When Same")
     void albumNameShouldNotChangeAndNotGenerateEvent() {
-        final Album album = Album.restore(ALBUM_ID, ALBUM_NAME, new ArtistId(ARTIST_ID), Set.of(new TrackId(TRACK_ID)));
-        album.changeAlbumName(ALBUM_NAME);
-        Optional<AlbumEvent> albumEvent = album.findLatestEvent();
+        final Album album = Album.restore(ALBUM_ID, DEFAULT_ALBUM_NAME, DEFAULT_ARTIST, Set.of(DEFAULT_TRACK));
+        album.changeAlbumName(DEFAULT_ALBUM_NAME);
 
+        Optional<AlbumEvent> albumEvent = album.findLatestEvent();
         assertFalse(albumEvent.isPresent());
     }
 
     @Test
     @DisplayName("Track Should Be Added To Album When Not Appear To Already Be")
-    void trackShouldBeAddedToEventWhenNotAdded() {
-        final Album album = Album.restore(ALBUM_ID, ALBUM_NAME, new ArtistId(ARTIST_ID), Set.of(new TrackId(TRACK_ID)));
-        final TrackId track = new TrackId(ANOTHER_TRACK_ID);
+    void trackShouldBeAddedWhenNotAdded() {
+        final Album album = Album.restore(ALBUM_ID, DEFAULT_ALBUM_NAME, DEFAULT_ARTIST, Set.of(DEFAULT_TRACK));
 
-        assertFalse(album.hasTrack(track));
-        album.addTrack(track);
+        assertFalse(album.hasTrack(ANOTHER_TRACK));
+        album.addTrack(ANOTHER_TRACK);
+
         final Optional<AlbumEvent> albumEvent = album.findLatestEvent();
-
         assertTrue(albumEvent.isPresent());
         assertEquals(TRACK_ADDED_EVENT, albumEvent.get().getClass());
+
         final TrackAddedEvent trackAddedEvent = (TrackAddedEvent) albumEvent.get();
         assertEquals(trackAddedEvent.aggregateId(), ALBUM_ID);
-        assertEquals(trackAddedEvent.track(), track);
+        assertEquals(trackAddedEvent.track(), ANOTHER_TRACK);
     }
 
     @Test
     @DisplayName("Track Should Not Be Added To Album When Already Added")
-    void trackShouldNotBeAddedToEventWhenAdded() {
-        final Album album = Album.restore(ALBUM_ID, ALBUM_NAME, new ArtistId(ARTIST_ID), Set.of(new TrackId(TRACK_ID)));
-        final TrackId track = new TrackId(TRACK_ID);
+    void trackShouldNotBeAddedAgain() {
+        final Album album = Album.restore(ALBUM_ID, DEFAULT_ALBUM_NAME, DEFAULT_ARTIST, Set.of(DEFAULT_TRACK));
 
-        assertTrue(album.hasTrack(track));
-        album.addTrack(track);
+        assertTrue(album.hasTrack(DEFAULT_TRACK));
+        album.addTrack(DEFAULT_TRACK);
+
         final Optional<AlbumEvent> albumEvent = album.findLatestEvent();
-
         assertFalse(albumEvent.isPresent());
     }
 
     @Test
     @DisplayName("Track Should Be Removed From Album When Already Added")
-    void trackShouldBeRemovedToEventWhenAdded() {
-        final Album album = Album.restore(ALBUM_ID, ALBUM_NAME, new ArtistId(ARTIST_ID), Set.of(new TrackId(TRACK_ID)));
-        final TrackId track = new TrackId(TRACK_ID);
+    void trackShouldBeRemovedWhenAdded() {
+        final Album album = Album.restore(ALBUM_ID, DEFAULT_ALBUM_NAME, DEFAULT_ARTIST, Set.of(DEFAULT_TRACK));
 
-        assertTrue(album.hasTrack(track));
-        album.removeTrack(track);
+        assertTrue(album.hasTrack(DEFAULT_TRACK));
+        album.removeTrack(DEFAULT_TRACK);
         final Optional<AlbumEvent> albumEvent = album.findLatestEvent();
 
         assertTrue(albumEvent.isPresent());
         assertEquals(TRACK_REMOVED_EVENT, albumEvent.get().getClass());
         final TrackRemovedEvent trackRemovedEvent = (TrackRemovedEvent) albumEvent.get();
         assertEquals(trackRemovedEvent.aggregateId(), ALBUM_ID);
-        assertEquals(trackRemovedEvent.track(), track);
+        assertEquals(trackRemovedEvent.track(), DEFAULT_TRACK);
     }
 
     @Test
     @DisplayName("Track Should Not Be Removed To Album When Already Added")
-    void trackShouldNotBeRemovedToEventWhenNotAdded() {
-        final Album album = Album.restore(ALBUM_ID, ALBUM_NAME, new ArtistId(ARTIST_ID), Set.of(new TrackId(TRACK_ID)));
-        final TrackId track = new TrackId(ANOTHER_TRACK_ID);
+    void trackShouldNotBeRemovedWhenNotAdded() {
+        final Album album = Album.restore(ALBUM_ID, DEFAULT_ALBUM_NAME, DEFAULT_ARTIST, Set.of(DEFAULT_TRACK));
 
-        assertFalse(album.hasTrack(track));
-        album.removeTrack(track);
+        assertFalse(album.hasTrack(ANOTHER_TRACK));
+        album.removeTrack(ANOTHER_TRACK);
+
         final Optional<AlbumEvent> albumEvent = album.findLatestEvent();
-
         assertFalse(albumEvent.isPresent());
     }
 
