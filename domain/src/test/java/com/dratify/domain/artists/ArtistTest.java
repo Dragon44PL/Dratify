d99781cf8d639eb8f15c0e10c83ad4edb7641e93
Dragon44PL <krystian.pieniak44@gmail.com>
2021-05-25 22:44:02 +0200

    Created ArtistTest

---
+++ domain/src/test/java/com/dratify/domain/artists/ArtistTest.java
@@ -0,0 +1,49 @@
+package com.dratify.domain.artists;
+
+import com.dratify.domain.artists.event.ArtistCreatedEvent;
+import com.dratify.domain.artists.event.ArtistEvent;
+import com.dratify.domain.artists.vo.ArtistType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+class ArtistTest {
+
+    private final UUID ARTIST_ID = UUID.randomUUID();
+    private final String ARTIST_NAME = "Artist";
+    private final ArtistType ARTIST_TYPE = ArtistType.ARTIST;
+
+    /**
+     *  Artist Events
+     */
+
+    private final Class<ArtistCreatedEvent> ARTIST_CREATED_EVENT = ArtistCreatedEvent.class;
+
+    @Test
+    @DisplayName("Artist Should Create Properly And Generate ArtistCreatedEvent")
+    void artistShouldCreateProperlyAndGenerateEvent() {
+        final Artist artist = Artist.create(ARTIST_ID, ARTIST_NAME, ARTIST_TYPE);
+
+        Optional<ArtistEvent> artistEvent = artist.findLatestEvent();
+        assertTrue(artistEvent.isPresent());
+        assertEquals(ARTIST_CREATED_EVENT, artistEvent.get().getClass());
+
+        final ArtistCreatedEvent artistCreatedEvent = (ArtistCreatedEvent) artistEvent.get();
+        assertEquals(ARTIST_ID, artistCreatedEvent.aggregateId());
+        assertEquals(ARTIST_NAME, artistCreatedEvent.name());
+        assertEquals(ARTIST_TYPE, artistCreatedEvent.artistType());
+    }
+
+    @Test
+    @DisplayName("Artist Should Restore Properly And Not Generate Event")
+    void artistShouldRestoreProperlyAndNotGenerateEvent() {
+        final Artist artist = Artist.restore(ARTIST_ID, ARTIST_NAME, ARTIST_TYPE);
+
+        Optional<ArtistEvent> artistEvent = artist.findLatestEvent();
+        assertFalse(artistEvent.isPresent());
+    }
+}
