7692e2193b3484e0e1dbb606cc9900311b5d8429
Dragon44PL <krystian.pieniak44@gmail.com>
2021-11-13 19:22:54 +0100

    Created VorbisDataRepository

--- player/player-adapter/player-adapter-repository/src/main/java/com/musiva/player/repository/VorbisDataRepository.java
+++ player/player-adapter/player-adapter-repository/src/main/java/com/musiva/player/repository/VorbisDataRepository.java
@@ -1,41 +1,27 @@
 package com.musiva.player.repository;
 
-import com.musiva.player.TrackData;
-import org.gagravarr.vorbis.VorbisAudioData;
+import com.musiva.player.VorbisTrackData;
+import com.musiva.player.communication.Decoder;
 import org.gagravarr.vorbis.VorbisFile;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
-public class VorbisDataRepository implements TrackDataRepository {
+public class VorbisDataRepository implements TrackDataRepository<VorbisTrackData> {
+
+    private final Decoder<VorbisFile, VorbisTrackData> vorbisDecoder;
+
+    public VorbisDataRepository(Decoder<VorbisFile, VorbisTrackData>  vorbisDecoder) {
+        this.vorbisDecoder = vorbisDecoder;
+    }
 
     @Override
-    public Optional<TrackData> findByFilename(Filename filename) {
-
-        try {
-
-            VorbisFile vorbisFile = new VorbisFile(new File(filename.toString()));
-            List<VorbisAudioData> data = new ArrayList<>();
-            VorbisAudioData currentPacket;
-
-            while((currentPacket = vorbisFile.getNextAudioPacket()) != null) {
-                data.add(currentPacket);
-            }
-
-            vorbisFile.close();
-
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
-            objectOutputStream.writeObject(data);
-            return Optional.of(new TrackData(byteArrayOutputStream.toByteArray()));
-
-        } catch (Exception e) {
+    public Optional<VorbisTrackData> findByFilename(Filename filename) {
+        try(VorbisFile vorbisFile = new VorbisFile(new File(filename.toString()))) {
+            return vorbisDecoder.decode(vorbisFile);
+        } catch (Exception e ) {
             return Optional.empty();
         }
-
     }
+
 }
