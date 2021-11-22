18ad5a7eb227c786634bf27613e105c37ee75cc7
Dragon44PL <krystian.pieniak44@gmail.com>
2021-11-22 23:55:16 +0100

    implemented 'resolveByteRange' method

--- player/player-adapter/player-adapter-repository/src/main/java/com/musiva/player/repository/cache/VorbisByteRangeResolver.java
+++ player/player-adapter/player-adapter-repository/src/main/java/com/musiva/player/repository/cache/VorbisByteRangeResolver.java
@@ -20,19 +20,41 @@ public class VorbisByteRangeResolver {
     public Optional<TrackData> resolveByteRange(VorbisTrackData vorbisTrackData, ByteRange byteRange) {
 
         final List<VorbisAudioData> vorbisAudioData = new ArrayList<>();
+        long start, end = 0;
 
-        long start = byteRange.begin();
-        long end = byteRange.end();
-
-        long current = 0;
         for(VorbisAudioData audioData : vorbisTrackData.vorbisAudioData()) {
-            long length = audioData.getData().length;
-            if(current > length) {
+            long currentPacketLength = audioData.getData().length;
+            start = end;
+            end += currentPacketLength;
+            int beginDiff = 0, endDiff = 0;
 
+            if(start >= byteRange.begin()) {
+                beginDiff = (int) Math.abs(start - byteRange.begin());
+            }
+
+            if(end >= byteRange.end()) {
+                endDiff = (int) Math.abs(end - byteRange.end());
+            }
+
+            if(beginDiff - endDiff > 0) {
+                VorbisAudioData created = resolveAudioData(audioData, beginDiff, endDiff);
+                vorbisAudioData.add(created);
             }
         }
 
         final VorbisTrackData destination = new VorbisTrackData(vorbisAudioData, vorbisTrackData.vorbisInfo(), vorbisTrackData.vorbisComments(), vorbisTrackData.vorbisSetup());
         return vorbisEncoder.encode(destination);
+    }
+
+    private VorbisAudioData resolveAudioData(VorbisAudioData vorbisAudioData, int beginOffset, int endOffset) {
+        byte[] vorbisBytes = vorbisAudioData.getData();
+        int length = vorbisBytes.length;
+
+        byte[] bytes = new byte[length - beginOffset - endOffset];
+        if (length - endOffset - beginOffset - 1 >= 0) {
+            System.arraycopy(vorbisBytes, beginOffset - 1, bytes, beginOffset - 1, length - endOffset - beginOffset - 1);
+        }
+
+        return new VorbisAudioData(bytes);
     }
 }
