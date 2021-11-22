334e65f59673abe78a5ef21e327739ddca2b9f50
Dragon44PL <krystian.pieniak44@gmail.com>
2021-11-22 23:24:50 +0100

    repackaged

--- info/info-domain/src/main/java/com/musiva/artists/event/ArtistCreatedEvent.java
+++ info/info-domain/src/main/java/com/musiva/artists/event/ArtistCreatedEvent.java
@@ -1,5 +1,10 @@
 package com.musiva.artists.event;
 
+import com.musiva.artists.vo.ArtistType;
+
+import java.time.Instant;
+import java.util.UUID;
+
 public record ArtistCreatedEvent(Instant occurredOn, UUID aggregateId, String name, ArtistType artistType) implements ArtistEvent {
 
     public ArtistCreatedEvent(UUID artistId, String name, ArtistType artistType) {
