ee51e2353da393aad2f17e6a356ebf2bcbb2d549
Dragon44PL <krystian.pieniak44@gmail.com>
2022-01-01 19:16:04 +0100

    Removed unused method

--- info/info-app/src/main/java/com/musiva/albums/album/AlbumQueryRepository.java
+++ info/info-app/src/main/java/com/musiva/albums/album/AlbumQueryRepository.java
@@ -1,10 +1,12 @@
 package com.musiva.albums.album;
 
+import com.musiva.albums.album.dto.AlbumDto;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface AlbumQueryRepository {
-    Optional<Album> findById(UUID id);
-    List<Album> findAll();
+    Optional<AlbumDto> findById(UUID id);
+    List<AlbumDto> findAll();
 }
